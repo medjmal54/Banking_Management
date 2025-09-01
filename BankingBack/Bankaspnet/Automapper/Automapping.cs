71f412ffae5aabb28445cf99cc7844715c980076
Mohamed Djmal <jmalmohamed@fsegs.u-sfax.tn>
2025-09-01 12:40:03 +0100

    Add files via upload

---
+++ BankingBack/Bankaspnet/Automapper/Automapping.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using BankingManagement.Models;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BankingManagement.Automapper
+{
+    public class Automapping : Profile
+    {
+        public Automapping()
+        {
+
+        }
+    }
+}

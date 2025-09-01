31ba555ebc8f05a2a9ee27b7b552a1a420ecda8b
Mohamed Djmal <jmalmohamed@fsegs.u-sfax.tn>
2025-09-01 12:41:03 +0100

    Add files via upload

---
+++ BankingBack/Bankaspnet/Program.cs
@@ -0,0 +1,42 @@
+using BankingManagement.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<BankingManagementContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("ConDB")));
+builder.Services.AddAutoMapper(typeof(Program));
+
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend",
+        policy => policy.WithOrigins("http://localhost:5173")
+                        .AllowAnyHeader()
+                        .AllowAnyMethod()
+                        .AllowCredentials() );
+});
+
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors("AllowFrontend");
+app.UseAuthorization();
+app.UseCors("AllowReactApp");
+
+app.MapControllers();
+
+app.Run();
